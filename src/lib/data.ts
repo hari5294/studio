063bf1a6dad3ff3784ac5617e8fb5e6125cf964d
Hari <lingasamyharivignesh@gmail.com>
2025-10-04 06:44:07 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Attempted to cal

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,6 +1,3 @@
-
-'use client';
-
 // All data functions have been moved to mock-db.ts to be used by both server and client components.
 // This file now re-exports them to maintain the import paths throughout the application.
 
