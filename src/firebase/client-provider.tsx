ac50bdfebfc6799d59c6f7f08aab5759116fa8ed
Hari <lingasamyharivignesh@gmail.com>
2025-10-05 08:10:03 +0000

    still same

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -10,7 +10,17 @@ import { Firestore, getFirestore } from 'firebase/firestore';
 
 import { FirebaseProvider, useAuth, useFirestore } from '.';
 import { User } from '@/lib/mock-data';
-import { firebaseConfig } from './config';
+
+// Hardcoded Firebase config to ensure it is always available on the client.
+const firebaseConfig = {
+  apiKey: "AIzaSyBKyV2J1wOkMQ2wEiGYRO6p675DzwL852o",
+  authDomain: "badgese-e258f.firebaseapp.com",
+  projectId: "badgese-e258f",
+  storageBucket: "badgese-e258f.appspot.com",
+  messagingSenderId: "877963955625",
+  appId: "1:877963955625:web:a2-1674fc078a0e30bc48b8",
+  measurementId: "G-5ZGXZRLGZL"
+};
 
 
 function initializeClientFirebase(): {
